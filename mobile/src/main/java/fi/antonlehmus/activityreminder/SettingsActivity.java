e534962355ffb001823ebac02eb0318ca98dee9a
Anton Lemus <anton.lehmus@gmail.com>
2016-04-16 21:20:43 +0300

    fix layout modularity and compatibility

--- mobile/src/main/java/fi/antonlehmus/activityreminder/SettingsActivity.java
+++ mobile/src/main/java/fi/antonlehmus/activityreminder/SettingsActivity.java
@@ -21,8 +21,6 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-
-
     }
 
     @Override
@@ -34,27 +32,5 @@ public class SettingsActivity extends AppCompatActivity {
     public void syncToWear(View view){
 
     }
-
-    GoogleApiClient mGoogleApiClient = new GoogleApiClient.Builder(this)
-            .addConnectionCallbacks(new GoogleApiClient.ConnectionCallbacks() {
-                @Override
-                public void onConnected(Bundle connectionHint) {
-                    Log.d(TAG, "onConnected: " + connectionHint);
-                    // Now you can use the Data Layer API
-                }
-                @Override
-                public void onConnectionSuspended(int cause) {
-                    Log.d(TAG, "onConnectionSuspended: " + cause);
-                }
-            })
-            .addOnConnectionFailedListener(new GoogleApiClient.OnConnectionFailedListener() {
-                @Override
-                public void onConnectionFailed(ConnectionResult result) {
-                    Log.d(TAG, "onConnectionFailed: " + result);
-                }
-            })
-            // Request access only to the Wearable API
-            .addApi(Wearable.API)
-            .build();
 
 }
