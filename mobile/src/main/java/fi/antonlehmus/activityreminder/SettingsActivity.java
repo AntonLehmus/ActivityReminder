891c07fa30007243032a7cddc21f3e5d11ae39f0
Anton Lemus <anton.lehmus@gmail.com>
2016-04-16 20:31:54 +0300

    add SettingsFragment. Remove old - suboptimal settings implementation

--- mobile/src/main/java/fi/antonlehmus/activityreminder/SettingsActivity.java
+++ mobile/src/main/java/fi/antonlehmus/activityreminder/SettingsActivity.java
@@ -1,19 +1,11 @@
 package fi.antonlehmus.activityreminder;
 
-import android.app.Dialog;
-import android.app.TimePickerDialog;
-import android.support.v4.app.DialogFragment;
+
+import android.content.SharedPreferences;
+import android.preference.Preference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.format.DateFormat;
-import android.util.Log;
-import android.view.View;
-import android.widget.TextView;
-import android.widget.TimePicker;
 
-import java.util.Calendar;
-
-import static fi.antonlehmus.activityreminder.R.id.silentStop;
 
 public class SettingsActivity extends AppCompatActivity {
 
@@ -21,77 +13,7 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-    }
-
-
-    public void setSilentStart(View view){
-        DialogFragment timeFragment = new StartTimePickerFragment();
-        timeFragment.show(getSupportFragmentManager(), "pickerStartTime");
-
-    }
-    public void setSilentStop(View view){
-        DialogFragment timeFragment = new StopTimePickerFragment();
-        timeFragment.show(getSupportFragmentManager(), "pickerStopTime");
-    }
-    public void setInterval(View view){
-
-    }
-    public void startReminder(View view){
-
-    }
-    public void stopReminder(View view){
 
     }
 
-    public static class StartTimePickerFragment extends DialogFragment
-            implements TimePickerDialog.OnTimeSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return it
-            return new TimePickerDialog(getActivity(), this, hour, minute,
-                    DateFormat.is24HourFormat(getActivity()));
-        }
-
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            TextView tv = (TextView) getActivity().findViewById(R.id.silentStart);
-            if(minute<10){
-                tv.setText(hourOfDay + ":0" + minute);
-            }
-            else {
-                tv.setText(hourOfDay + ":" + minute);
-            }
-        }
-    }
-
-    public static class StopTimePickerFragment extends DialogFragment
-            implements TimePickerDialog.OnTimeSetListener {
-
-        @Override
-        public Dialog onCreateDialog(Bundle savedInstanceState) {
-            // Use the current time as the default values for the picker
-            final Calendar c = Calendar.getInstance();
-            int hour = c.get(Calendar.HOUR_OF_DAY);
-            int minute = c.get(Calendar.MINUTE);
-
-            // Create a new instance of TimePickerDialog and return it
-            return new TimePickerDialog(getActivity(), this, hour, minute,
-                    DateFormat.is24HourFormat(getActivity()));
-        }
-
-        public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
-            TextView tv = (TextView) getActivity().findViewById(R.id.silentStop);
-            if(minute<10){
-                tv.setText(hourOfDay + ":0" + minute);
-            }
-            else {
-                tv.setText(hourOfDay + ":" + minute);
-            }
-        }
-    }
 }
---
+++ mobile/src/main/java/fi/antonlehmus/activityreminder/SettingsFragment.java
@@ -0,0 +1,27 @@
+package fi.antonlehmus.activityreminder;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.preference.Preference;
+import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class SettingsFragment extends PreferenceFragment {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Load the preferences from an XML resource
+        addPreferencesFromResource(R.xml.preferences);
+
+    }
+
+}
