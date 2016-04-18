6a2bbcfdffedf8afc1016f86d7cecf8e1edce2c7
Anton Lemus <anton.lehmus@gmail.com>
2016-04-18 14:25:39 +0300

    asetukset lähtee puhelimesta muttei tule kelloon

--- mobile/src/main/java/fi/antonlehmus/activityreminder/SettingsActivity.java
+++ mobile/src/main/java/fi/antonlehmus/activityreminder/SettingsActivity.java
@@ -4,7 +4,6 @@ package fi.antonlehmus.activityreminder;
 
 
 import android.content.SharedPreferences;
-import android.net.Uri;
 import android.preference.PreferenceManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -17,7 +16,6 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.common.api.PendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.wearable.DataApi;
 import com.google.android.gms.wearable.PutDataMapRequest;
@@ -50,11 +48,6 @@ public class SettingsActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        /*
-        getFragmentManager().beginTransaction()
-                .add(android.R.id.content, new SettingsFragment())
-                .commit();
-        */
 
         mGoogleApiClient = new GoogleApiClient.Builder(this)
                 .addApi(Wearable.API)
@@ -108,16 +101,37 @@ public class SettingsActivity extends AppCompatActivity
     }
 
     public void btnSyncToWear(View view){
-        Log.d(TAG,"nappi");
         syncToWear();
     }
 
 
-    private void buildWearableOnlyNotification(String title, String content, String path) {
+    private void syncToWear() {
+
+        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean resumeOnReboot = sharedPref.getBoolean(getString(R.string.key_resume_on_boot),true);
+        long silent_start = sharedPref.getLong(getString(R.string.key_silent_start),72000000);
+        long silent_stop = sharedPref.getLong(getString(R.string.key_silent_stop),28800000);
+        int remind_interval = Integer.valueOf(sharedPref.getString(getString(R.string.key_remind_interval),"60"));
+        int step_trigger = Integer.valueOf(sharedPref.getString(getString(R.string.key_step_trigger),"15"));
+
+        //note:times are in GMT
+        Log.d(TAG,"\n");
+        Log.d(TAG,"resume on reboot:"+resumeOnReboot);
+        Log.d(TAG,"silent start:"+(silent_start*0.000000277778));
+        Log.d(TAG,"silent stop:"+(silent_stop*0.000000277778));
+        Log.d(TAG,"remind interval:"+remind_interval);
+        Log.d(TAG,"step trigger:"+step_trigger);
+
         if (mGoogleApiClient.isConnected()) {
-            PutDataMapRequest putDataMapRequest = PutDataMapRequest.create(path);
-            putDataMapRequest.getDataMap().putString(REBOOT_KEY, content);
-            putDataMapRequest.getDataMap().putString(SILENT_START_KEY, title);
+
+            Log.d(TAG,"connected to wear. Trying to send.");
+
+            PutDataMapRequest putDataMapRequest = PutDataMapRequest.create("/preferences");
+            putDataMapRequest.getDataMap().putBoolean(REBOOT_KEY, resumeOnReboot);
+            putDataMapRequest.getDataMap().putLong(SILENT_START_KEY, silent_start);
+            putDataMapRequest.getDataMap().putLong(SILENT_STOP_KEY, silent_stop);
+            putDataMapRequest.getDataMap().putInt(REMIND_INTERVAL_KEY, remind_interval);
+            putDataMapRequest.getDataMap().putInt(STEP_TRIGGER_KEY, step_trigger);
             PutDataRequest request = putDataMapRequest.asPutDataRequest();
             request.setUrgent();
             Wearable.DataApi.putDataItem(mGoogleApiClient, request)
@@ -125,31 +139,18 @@ public class SettingsActivity extends AppCompatActivity
                         @Override
                         public void onResult(DataApi.DataItemResult dataItemResult) {
                             if (!dataItemResult.getStatus().isSuccess()) {
-                                Log.e(TAG, "buildWatchOnlyNotification(): Failed to set the data, "
+                                Log.e(TAG, "syncToWear(): Failed to set the data, "
                                         + "status: " + dataItemResult.getStatus().getStatusCode());
+                            }
+                            else{
+                                Log.d(TAG,"Successfully sent settings to wear");
                             }
                         }
                     });
         } else {
-            Log.e(TAG, "buildWearableOnlyNotification(): no Google API Client connection");
+            Log.e(TAG, "syncToWear(): no Google API Client connection");
         }
-    }
 
-    private void syncToWear() {
-
-
-        SharedPreferences sharedPref = PreferenceManager.getDefaultSharedPreferences(this);
-        boolean resumeOnReboot = sharedPref.getBoolean(getString(R.string.key_resume_on_boot),true);
-        long silent_start = sharedPref.getLong(getString(R.string.key_silent_start),72000000);
-        long silent_stop = sharedPref.getLong(getString(R.string.key_silent_stop),28800000);
-        int remind_interval =  sharedPref.getInt(getString(R.string.key_remind_interval),60);
-        int step_trigger = sharedPref.getInt(getString(R.string.key_step_trigger),15);
-
-        Log.d(TAG,"resume on reboot:"+resumeOnReboot);
-        Log.d(TAG,"silent start:"+(silent_start*0.000000277778));
-        Log.d(TAG,"silent stop:"+(silent_stop*0.000000277778));
-        Log.d(TAG,"remind interval:"+remind_interval);
-        Log.d(TAG,"step trigger:"+step_trigger);
     }
 
 }
--- wear/src/main/java/fi/antonlehmus/activityreminder/StepReaderService.java
+++ wear/src/main/java/fi/antonlehmus/activityreminder/StepReaderService.java
@@ -1,8 +1,9 @@
 package fi.antonlehmus.activityreminder;
 
 import android.app.AlarmManager;
+import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
+
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -10,13 +11,31 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.net.Uri;
 import android.os.AsyncTask;
+import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.wearable.DataApi;
+import com.google.android.gms.wearable.DataEvent;
+import com.google.android.gms.wearable.DataEventBuffer;
+import com.google.android.gms.wearable.DataMap;
+import com.google.android.gms.wearable.DataMapItem;
+import com.google.android.gms.wearable.Wearable;
+import com.google.android.gms.wearable.WearableListenerService;
+
 import java.util.Calendar;
 
-public class StepReaderService extends Service implements SensorEventListener {
+import static com.google.android.gms.wearable.PutDataRequest.WEAR_URI_SCHEME;
+
+public class StepReaderService extends WearableListenerService
+        implements SensorEventListener,  GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        ResultCallback<DataApi.DeleteDataItemsResult> {
 
     private final int DEBUG_INTERVAL = 30;
 
@@ -31,6 +50,13 @@ public class StepReaderService extends Service implements SensorEventListener {
     private final long DEFAULT_CYCLE_TIME = 3600000; //60min in milliseconds
     private final int DEFAULT_SILENT_START = 20;
     private final int DEFAULT_SILENT_STOP = 8;
+
+    private static final String REBOOT_KEY = "resume_on_reboot";
+    private static final String SILENT_START_KEY = "silent_start";
+    private static final String SILENT_STOP_KEY = "silent_stop";
+    private static final String STEP_TRIGGER_KEY = "steps_trigger";
+    private static final String REMIND_INTERVAL_KEY = "remind_interval";
+
 
 
     private float steps = 0;
@@ -47,6 +73,13 @@ public class StepReaderService extends Service implements SensorEventListener {
     private Sensor sensor = null;
     private Calendar calendarSilentStart = null;
     private Calendar calendarSilentStop = null;
+    private GoogleApiClient mGoogleApiClient;
+
+    private static boolean resumeOnReboot;
+    private static long silent_start;
+    private static long silent_stop ;
+    private static int remind_interval;
+    private static int step_trigger;
 
 
 
@@ -80,8 +113,6 @@ public class StepReaderService extends Service implements SensorEventListener {
         sensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
         sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_UI);
 
-        Log.d(LOG_TAG,"_STARTED_");
-
         return START_STICKY;
     }
 
@@ -105,29 +136,62 @@ public class StepReaderService extends Service implements SensorEventListener {
         sensorManager.unregisterListener(this);
     }
 
-    private void startNewCycle(){
-        SharedPreferences sharedPref = getSharedPreferences(PERSISTENT_STEPS, Context.MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPref.edit();
-        editor.putFloat(SAVED_STEPS, steps);
-        editor.putLong(CYCLE_START_TIME, System.currentTimeMillis());
-        editor.apply();
-
-        if(steps-oldSteps<DEFAULT_STEP_COUNT_TRIGGER){
-            notifyUser();
-        }
-
-        Log.d(LOG_TAG,"New cycle started!");
-    }
-
-    private void notifyUser(){
-        Intent notifyIntent = new Intent();
-        notifyIntent.setAction("fi.antonlehmus.activityreminde.notifyUser");
-        sendBroadcast(notifyIntent);
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addApi(Wearable.API)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .build();
     }
 
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onDataChanged(DataEventBuffer dataEvents) {
+
+        for (DataEvent dataEvent : dataEvents) {
+            Log.d(LOG_TAG,dataEvent.toString());
+            if (dataEvent.getType() == DataEvent.TYPE_CHANGED) {
+                DataMap dataMap = DataMapItem.fromDataItem(dataEvent.getDataItem()).getDataMap();
+                resumeOnReboot = dataMap.getBoolean(REBOOT_KEY);
+                silent_start = dataMap.getLong(SILENT_START_KEY);
+                silent_stop = dataMap.getLong(SILENT_STOP_KEY);
+                remind_interval = Integer.valueOf(dataMap.getString(REMIND_INTERVAL_KEY));
+                step_trigger = Integer.valueOf(dataMap.getString(STEP_TRIGGER_KEY));
+
+                //note:times are in GMT
+                Log.d(LOG_TAG,"resume on reboot:"+resumeOnReboot);
+                Log.d(LOG_TAG,"silent start:"+(silent_start*0.000000277778));
+                Log.d(LOG_TAG,"silent stop:"+(silent_stop*0.000000277778));
+                Log.d(LOG_TAG,"remind interval:"+remind_interval);
+                Log.d(LOG_TAG,"step trigger:"+step_trigger);
+            }
+        }
+    }
+
+    @Override
+    public void onConnected(Bundle bundle) {
+        final Uri dataItemUri =
+                new Uri.Builder().scheme(WEAR_URI_SCHEME).path("/preferences").build();
+        if (Log.isLoggable(LOG_TAG, Log.DEBUG)) {
+            Log.d(LOG_TAG, "Deleting Uri: " + dataItemUri.toString());
+        }
+        Wearable.DataApi.deleteDataItems(
+                mGoogleApiClient, dataItemUri).setResultCallback(this);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+    }
+
+    @Override
+    public void onResult(DataApi.DeleteDataItemsResult deleteDataItemsResult) {
+        Log.d(LOG_TAG,"disconnecting");
+        mGoogleApiClient.disconnect();
     }
 
 
@@ -153,6 +217,25 @@ public class StepReaderService extends Service implements SensorEventListener {
         }
     }
 
+    private void startNewCycle(){
+        SharedPreferences sharedPref = getSharedPreferences(PERSISTENT_STEPS, Context.MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPref.edit();
+        editor.putFloat(SAVED_STEPS, steps);
+        editor.putLong(CYCLE_START_TIME, System.currentTimeMillis());
+        editor.apply();
+
+        if(steps-oldSteps<DEFAULT_STEP_COUNT_TRIGGER){
+            notifyUser();
+        }
+
+        Log.d(LOG_TAG,"New cycle started!");
+    }
+
+    private void notifyUser(){
+        Intent notifyIntent = new Intent();
+        notifyIntent.setAction("fi.antonlehmus.activityreminde.notifyUser");
+        sendBroadcast(notifyIntent);
+    }
 
     private void cancelAlarm(){
         scheduler.cancel(scheduledIntent);
